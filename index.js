12552e29f98239458c745820bc10df59facd235e
Calvin Jung <ediyacoo@gmail.com>
2015-07-16 13:11:13 -0700

    allow giving options with existing canvas
    if users would like to give options (such as radius) with the existing
    canvas while creating a HEATMAP obj, the codes should allow users to do
    that.

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ function createCanvas (width, height) {
     }
 }
 
-var exports = module.exports = function (canvas) {
-    var opts = {};
-    
+var exports = module.exports = function (canvas, opts) {
+    opts = opts || {};
+
     if (typeof canvas !== 'object') {
         canvas = createCanvas(arguments[0], arguments[1]);
         opts = arguments[2] || {};
@@ -42,11 +42,11 @@ exports.Heat = Heat;
 
 function Heat (canvas, opts) {
     if (!opts) opts = {};
-    
+
     this.canvas = canvas;
     this.width = canvas.width;
     this.height = canvas.height;
-    
+
     this.alphaCanvas = createCanvas(this.width, this.height);
     this.radius = opts.radius || 20;
     this.threshold = opts.threshold || 0;
@@ -55,15 +55,15 @@ function Heat (canvas, opts) {
 
 Heat.prototype.scale = function (x, y) {
     if (y === undefined) y = x;
-    
+
     this.scalar.x = x;
     this.scalar.y = y;
-    
+
     this.canvas.width = this.width * x;
     this.canvas.height = this.height * y;
-    
+
     this.canvas.getContext('2d').scale(x, y);
-    
+
     return this;
 };
 
@@ -74,16 +74,16 @@ Heat.prototype.addPoint = function (x, y, params) {
     }
     if (!params) params = {};
     var radius = params.radius || this.radius;
-    
+
     var g = ctx.createRadialGradient(x, y, 0, x, y, radius);
     var a = params.weight || (1 / 10);
-    
+
     g.addColorStop(0, 'rgba(255,255,255,' + a + ')');
     g.addColorStop(1, 'rgba(255,255,255,0)');
-    
+
     ctx.fillStyle = g;
     ctx.fillRect(x - radius, y - radius, radius * 2, radius * 2);
-    
+
     return this;
 };
 
@@ -91,18 +91,18 @@ Heat.prototype.draw = function () {
     var width = this.canvas.width;
     var height = this.canvas.height;
     var ctx = this.alphaCanvas.getContext('2d');
-    
+
     var values = ctx.getImageData(0, 0, this.width, this.height);
     var heat = ctx.createImageData(width, height);
-    
+
     for (var hy = 0; hy < height; hy++) {
         var vy = Math.floor(hy / this.scalar.y);
-        
+
         for (var hx = 0; hx < width; hx++) {
             var vx = Math.floor(hx / this.scalar.x);
             var vi = 4 * (vy * this.width + vx);
             var hi = 4 * (hy * width + hx);
-            
+
             var v = values.data[vi + 3];
             if (v > this.threshold) {
                 var theta = (1 - v / 255) * 270;
@@ -114,8 +114,8 @@ Heat.prototype.draw = function () {
             }
         }
     }
-    
+
     this.canvas.getContext('2d').putImageData(heat, 0, 0);
-    
+
     return this;
 };
